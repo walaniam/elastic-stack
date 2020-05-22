cdbd20949a5ae0c9d8891e6c2e34d01a1acb5721
walaniam <mariusz.walania@gmail.com>
2020-05-22 12:36:17 +0200

    Start / stop elastic fix

--- elastic-run.sh
+++ elastic-run.sh
@@ -6,5 +6,8 @@ if [[ "$(docker images -q $ELASTIC_IMAGE 2> /dev/null)" == "" ]]; then
   docker pull $ELASTIC_IMAGE
 fi
 
-docker network create --driver bridge $NETWORK || true
+if [[ "$(docker network ls |grep $NETWORK 2> /dev/null)" == "" ]]; then
+  docker network create --driver bridge $NETWORK || true
+fi
+
 docker run --rm -d -p 9200:9200 -p 9300:9300 --network=$NETWORK --name elasticsearch -e "discovery.type=single-node" $ELASTIC_IMAGE
