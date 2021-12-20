59eea224e7a7886c25bc9f14cab4384dbf94c43c
Mariusz Walania <mariusz.walania@gmail.com>
2021-12-20 20:00:00 +0100

    latest elastic

--- scripts/_elk-conf.sh
+++ scripts/_elk-conf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ELASTIC_IMAGE=docker.elastic.co/elasticsearch/elasticsearch:7.5.0
-KIBANA_IMAGE=docker.elastic.co/kibana/kibana:7.5.0
+ELASTIC_IMAGE=docker.elastic.co/elasticsearch/elasticsearch:7.16.1
+KIBANA_IMAGE=docker.elastic.co/kibana/kibana:7.16.1
 NETWORK=elastic-stack_elastic
 
 mkdir -p ~/dev-data/elastic/data
